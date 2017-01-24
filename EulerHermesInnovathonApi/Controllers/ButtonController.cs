8e4a4e5988f5bd4b9c8566f75cfdcdcdc2adbd07
Mick Philippon <mick.philippon@cellenza.com>
2017-01-24 10:02:45 +0100

    Api for blue button

---
+++ EulerHermesInnovathonApi/Controllers/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using EulerHermesInnovathonApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EulerHermesInnovathonApi.Controllers
+{
+    public class ButtonController : ApiController
+    {
+        [HttpGet]
+        [Route("/category/all")]
+        public IEnumerable<Category> GetEmergencyCategories()
+        {
+            return Category.GetCategoryList();
+        }
+
+        [HttpGet]
+        [Route("/category/{id}")]
+        public Category GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ EulerHermesInnovathonApi/Models/Category.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EulerHermesInnovathonApi.Models
+{
+    public class Category
+    {
+        private static int nextId = 0;
+        private static readonly IEnumerable<Category> _CategoryList = new List<Category>()
+        {
+            new Category("", ""),
+        };
+
+        public static IEnumerable<Category> GetCategoryList()
+        {
+            return _CategoryList;
+        }
+
+        public Category() { this.CategoryId = nextId++; }
+        public Category(string name, string description)
+        {
+            this.CategoryId = nextId++;
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
